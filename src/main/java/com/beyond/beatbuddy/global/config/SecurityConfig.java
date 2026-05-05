f480e15cdbfb793f0dd10c6e9f2b2b46dc27fdd8
huisu73 <hhisu102@gmail.com>
2026-05-06 00:37:09 +0900

    Add beatbuddy.local to CORS allowed origins

--- src/main/java/com/beyond/beatbuddy/global/config/SecurityConfig.java
+++ src/main/java/com/beyond/beatbuddy/global/config/SecurityConfig.java
@@ -93,6 +93,7 @@ public class SecurityConfig {
         // 어떤 주소에서 오는 요청 허용할지
         config.addAllowedOrigin("http://localhost:5173"); // Vue 기본 포트
         config.addAllowedOrigin("http://161.33.207.230");
+        config.addAllowedOrigin("http://beatbuddy.local:30080");
         // 어떤 HTTP 메서드 허용할지
         config.addAllowedOriginPattern("*.trycloudflare.com");
 
